792a56ac431b9b0166ea21415137862919cdfc4f
Steffano99 <101764484+Steffano99@users.noreply.github.com>
2024-10-16 17:47:43 +0000

    implementation of first part of splash screen for the connectify chat app

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,8 @@ import 'package:connectify_chat_app/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Connectify',
-      theme: ThemeData(
-          // scaffoldBackgroundColor:
-          ),
-      home: SplashScreen(
-        key: UniqueKey(),
-        onInitializationComplete: () {},
-      ),
-    );
-  }
+  runApp(SplashScreen(
+    key: UniqueKey(),
+    onInitializationComplete: () {},
+  ));
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -16,6 +16,23 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'Connectify',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white, // Set scaffold background color
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            width: 250,
+            height: 250,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/images/connectify.png'))),
+          ),
+        ),
+      ),
+    );
   }
 }
